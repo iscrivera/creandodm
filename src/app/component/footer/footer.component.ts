972b228a0a7b5c5355b27c904a3c3d4ff22b4776
ariverav <isc.rivera@hotmail.com>
2020-03-20 12:36:34 -0600

    home

---
+++ src/app/component/footer/footer.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { messages } from 'src/app/constants/home';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  
+  messages = messages;
+  constructor() { }
+
+  ngOnInit(): void {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  }
+
+}
